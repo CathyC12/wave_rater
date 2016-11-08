0961311e2ba9ce49c0eb8ab9d22c40c3bf715f27
bunzybimx2 <jovellynanne11@gmail.com>
2016-11-08 16:24:35 +0000

    Added Login Section
    Added the login section (section does not work yet)
    Also created a library folder to hold the php files for the login.
    Changed the navigation of "about" to "login" , added script tag with the
    processLogin to connect the php with the html page.

---
+++ workspace/startbootstrap-creative-gh-pages/library/config.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  This is your configuration file and should include the names of your database and the access
+  credentials for gaining access to your database...
+*/
+    $dbname = "DataBase Name";
+  	$hostname = " Name of the Host ";
+  	$username = "Username that you are connecting with";
+  	$password = "The Password that authenticates the above username";
+?>
